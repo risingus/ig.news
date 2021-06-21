9589001c00fa53ceca7091adfce6b11e6f530f79
Gustavo <gustavo.lima@nd.com.br>
2021-06-21 11:02:15 -0300

    Feat: 1. Instaled react-icons. 2. Created style for home page.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 
 import Head from 'next/head';
+import {Header} from '../components/Header';
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>My new cool app</title>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"></link>
       </Head>
+      <Header/>
       <Component {...pageProps} />
     </>
   )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default class MyDocument extends Document {
     return (
     <Html>
       <Head/>
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"></link>
+      <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         <body>
           <Main />
           <NextScript />
