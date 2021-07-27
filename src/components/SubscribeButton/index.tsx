be8864c4880a6f0cc401fc273f779ea68acffcda
Gustavo <gustavo.it@outlook.com>
2021-07-27 13:16:38 -0300

    Feature
    End of configuration of payment with stripe.

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
 import { signIn, useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
+import {api} from '../../services/api';
+import {getStripeJs} from '../../services/stripe-js';
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -9,10 +11,22 @@ export function SubscribeButton({priceId}: SubscribeButtonProps) {
 
   const [session] = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn('github')
       return;
+    }
+
+    try { 
+      const response = await api.post('/subscribe');
+
+      const {sessionId} = response.data;
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({sessionId})
+    } catch (err) {
+      alert(err.message);
     }
     
     
